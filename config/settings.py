c4a9cd7e070b4cd22bd77bc24c325f39aa87c806
Roméo Tonato <teddywillis222@gmail.com>
2026-05-16 16:38:39 +0100

    fait

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'Africa/Porto-Novo'
 
